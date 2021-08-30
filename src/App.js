127595c2a7b7d17d4ca6e258e0d91218dd39b962
ismailcileli <ismlcll@hotmail.com>
2021-08-30 20:11:58 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="db1d5bcf-0837-4e44-8329-6e9042e07a85"
+      userName="ismailcileli"
+      userSecret="123123"
+      renderChatFeed= {(chatAppProps) => <ChatFeed {...chatAppProps }/>}
+    />
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
